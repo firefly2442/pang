6c4a24112051c0e3219404f47c384525a1e31647
firefly2442 <firefly2442@gmail.com>
2013-05-18 00:25:50 -0500

    minor code formatting

--- src/AIPaddle.cpp
+++ src/AIPaddle.cpp
@@ -15,15 +15,17 @@ AIPaddle::AIPaddle() :
     _velocity(0),
     _maxVelocity(600.0f)
 {
+	// constructor
     Load("resources/paddle.png");
     assert(IsLoaded());
 
-    GetSprite().setOrigin(GetSprite().getLocalBounds().width /2, GetSprite().getLocalBounds().height / 2);
+    GetSprite().setOrigin(GetSprite().getLocalBounds().width/2, GetSprite().getLocalBounds().height/2);
 }
 
 
 AIPaddle::~AIPaddle()
 {
+	//destructor
 }
 
 void AIPaddle::Draw(sf::RenderWindow & rw)
@@ -58,8 +60,7 @@ void AIPaddle::Update(float elapsedTime)
 
     sf::Vector2f pos = this->GetPosition();
 
-    if(pos.x  <= GetSprite().getLocalBounds().width/2
-            || pos.x >= (Game::SCREEN_WIDTH - GetSprite().getLocalBounds().width/2))
+    if(pos.x  <= GetSprite().getLocalBounds().width/2 || pos.x >= (Game::SCREEN_WIDTH - GetSprite().getLocalBounds().width/2))
     {
         _velocity = -_velocity; // Bounce by current velocity in opposite direction
     }
--- src/PlayerPaddle.cpp
+++ src/PlayerPaddle.cpp
@@ -19,7 +19,7 @@ PlayerPaddle::PlayerPaddle() :
     Load("resources/paddle.png");
     assert(IsLoaded());
 
-    GetSprite().setOrigin(GetSprite().getLocalBounds().width /2, GetSprite().getLocalBounds().height / 2);
+    GetSprite().setOrigin(GetSprite().getLocalBounds().width/2, GetSprite().getLocalBounds().height/2);
 }
 
 
@@ -66,8 +66,7 @@ void PlayerPaddle::Update(float elapsedTime)
 
     sf::Vector2f pos = this->GetPosition();
 
-    if (pos.x  < GetSprite().getLocalBounds().width/2
-            || pos.x > (Game::SCREEN_WIDTH - GetSprite().getLocalBounds().width/2))
+    if (pos.x  < GetSprite().getLocalBounds().width/2 || pos.x > (Game::SCREEN_WIDTH - GetSprite().getLocalBounds().width/2))
     {
         _velocity = -_velocity; // Bounce by current velocity in opposite direction
     }
